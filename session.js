a0a019f54bd12c51fc03ddb2a79731c78e9ee7d4
vercel-24-7 <rasathemi@gmail.com>
2025-12-29 19:16:22 +0530

    Update default SESSION_ID value

--- session.js
+++ session.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = "true") {
 module.exports = {
   SESSION_ID:
     process.env.SESSION_ID ||
-    "XPROVerce~1LtB6vUeq8",
+    "XPROVerce~ZIrTDVqxkA",
   BOT_NUMBER:
     process.env.BOT_NUMBER || "1",
   PASSWORD: 
